f3b03687eabe017efedc573546448672e6e15cf9
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-01 19:44:04 +0530

    find the largest element O(N2) Selection Sort

--- ArrayEasy/src/findthelargestbruteforce.java
+++ ArrayEasy/src/findthelargestbruteforce.java
@@ -3,8 +3,16 @@ import java.util.Arrays;
 public class findthelargestbruteforce {
     static void main() {
         int arr[] = {13,46,24,52,20,9};
-        Arrays.sort(arr);
+        for(int i=0;i<arr.length-1;i++) {
+            for(int j=i+1;j<arr.length;j++) {
+                if(arr[i]>arr[j]) {
+                    int temp =  arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
         System.out.println(Arrays.toString(arr));
-        System.out.println("The Largest Element : " + arr[arr.length-1]);
+        System.out.println("The maximum number is " + arr[arr.length-1]);
     }
 }
