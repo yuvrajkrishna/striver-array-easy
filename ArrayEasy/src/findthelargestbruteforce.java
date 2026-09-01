c2e6f9b67be43224a37d02a60b7f3af69862e28c
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-01 19:44:55 +0530

    find the largest 0(N2) Bubble Sort

--- ArrayEasy/src/findthelargestbruteforce.java
+++ ArrayEasy/src/findthelargestbruteforce.java
@@ -3,12 +3,13 @@ import java.util.Arrays;
 public class findthelargestbruteforce {
     static void main() {
         int arr[] = {13,46,24,52,20,9};
-        for(int i=0;i<arr.length-1;i++) {
-            for(int j=i+1;j<arr.length;j++) {
-                if(arr[i]>arr[j]) {
-                    int temp =  arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        for(int i = 0 ; i < arr.length-1; i++){
+            boolean swapped = false;
+            for(int j = 0; j < arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
