06458235e8a2f628d33c25364b944a09c4ffb895
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-01 20:26:43 +0530

    optimized check if sorted

--- ArrayEasy/src/checkifarrayissorted.java
+++ ArrayEasy/src/checkifarrayissorted.java
@@ -1,10 +1,11 @@
 public class checkifarrayissorted {
     static void main() {
-        int array[] = {1,1,3,2,5};
-        for(int i = 1; i < array.length ; i++){
-            if(array[i] < array[i-1]){
-                System.out.println("Not sorted");
-                return;
+        int array[] = {1,1,2,2,5};
+        for(int i = 0; i < array.length-1; i++){
+                if(array[i] > array[i+1]){
+                    System.out.println("Not sorted");
+                    return;
+
             }
         }
         System.out.println("Sorted array");
