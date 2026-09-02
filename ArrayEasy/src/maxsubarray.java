dd06f14144f4f3ee0ddecc1e58f42eeebb65a02d
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-02 19:47:33 +0530

    maxsubarray

---
+++ ArrayEasy/src/maxsubarray.java
@@ -0,0 +1,16 @@
+public class maxsubarray {
+    static void main() {
+        int arr[] = {2,3,5,-2,7,-4};
+        int max = 0 ;
+        for(int i = 0 ; i < arr.length; i++){
+            for(int j = i; j < arr.length; j++){
+                int count = 0;
+                for(int k = i ; k < j; k++){
+                    count += arr[k];
+                }
+                max = Math.max(max, count);
+            }
+        }
+        System.out.println(max);
+    }
+}
